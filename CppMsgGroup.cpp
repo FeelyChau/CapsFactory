8f9499587b8225e89d36c64b8c4d775901327514
feely <feely@outlook.com>
2018-11-26 14:14:01 +0800

    fix:  function get_msg_type code bug

--- CppMsgGroup.cpp
+++ CppMsgGroup.cpp
@@ -23,7 +23,7 @@ string CppMsgGroup::create_code_string() {
                                            "  if (Caps::parse(buff, buff_len, caps) != CAPS_SUCCESS)\n"
                                            "    goto ERROR;\n"
                                            "  int32_t msg_type;\n"
-                                           "  if (caps.read(msg_type) != CAPS_SUCCESS)\n"
+                                           "  if (caps->read(msg_type) != CAPS_SUCCESS)\n"
                                            "    goto ERROR;\n"
                                            "  return static_cast<MessageType>(msg_type);\n"
                                            "ERROR:\n"
